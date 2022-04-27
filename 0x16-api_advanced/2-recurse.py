c50bde47e5b7c3ec3add19eadb877d4d7e8782cf
Joy879 <joywanjiru879@gmail.com>
2022-04-26 18:20:11 -0700

    Api advanced

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+a recursive function that queries the Reddit API and returns a list containing
+the titles of all hot articles for a given subreddit. If no results are found
+for the given subreddit, the function should return None.
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[]):
+    """
+    returns a list containing the titles of all hot articles
+    for a given subreddit
+    """
+    if type(subreddit) is list:
+        url = "https://api.reddit.com/r/{}?sort=hot".format(subreddit[0])
+        url = "{}&after={}".format(url, subreddit[1])
+    else:
+        url = "https://api.reddit.com/r/{}?sort=hot".format(subreddit)
+        subreddit = [subreddit, ""]
+    headers = {'User-Agent': 'CustomClient/1.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return (None)
+    response = response.json()
+    if "data" in response:
+        data = response.get("data")
+        if not data.get("children"):
+            return (hot_list)
+        for post in data.get("children"):
+            hot_list += [post.get("data").get("title")]
+        if not data.get("after"):
+            return (hot_list)
+        subreddit[1] = data.get("after")
+        recurse(subreddit, hot_list)
+        if hot_list[-1] is None:
+            del hot_list[-1]
+        return (hot_list)
+    else:
+        return (None)
