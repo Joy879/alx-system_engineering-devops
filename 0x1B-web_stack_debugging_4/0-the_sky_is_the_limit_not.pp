3a512497ee1df97c2f18d0d737273517efcbf636
Joy Wanjiru <70502261+Joy879@users.noreply.github.com>
2022-05-07 16:48:00 +0300

    Web Stack Debugging

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,6 @@
 # fix nginx to accept and serve more requests
 
-exec { 'nginx requests limit':
-  command => 'sed -i "s/15/1000/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-}
-
-#Restart NginX
-exec { 'restart nginx':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+exec {'modify max open files limit setting':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
